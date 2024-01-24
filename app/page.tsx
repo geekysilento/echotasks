c11e9b2c7dd8a970e2c4014a230e57d144cb644b
geekysilento <adityakarna.ak@gmail.com>
2024-01-24 20:43:30 +0545

    added signup page n fixed styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Board from "@/components/Board";
 import Header from "@/components/Header";
 import Signin from "@/components/Signin";
+import Signup from "@/components/Signup";
 
 export default function Home() {
   return (
     <main>
-      <Signin/>
+      <Signup/>
       {/* <Header />
       <Board /> */}
     </main>
